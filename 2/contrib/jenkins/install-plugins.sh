e22280dc7078f13c9596945f581b8b6ff5ff61e3
Vinu Kochukuttan <kevy.vinu@gmail.com>
2025-04-23 11:41:18 +0530

    Revert "JKNS-637: Remove deprecated references"

--- 2/contrib/jenkins/install-plugins.sh
+++ 2/contrib/jenkins/install-plugins.sh
@@ -5,7 +5,7 @@
 #
 # Usage:
 #
-# FROM registry.redhat.io/ocp-tools-4/jenkins-rhel9:v4.17.0
+# FROM openshift/jenkins-2-centos7
 # COPY plugins.txt /plugins.txt
 # RUN /usr/local/bin/install-plugins.sh /plugins.txt
 #
